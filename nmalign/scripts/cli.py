aacd421169e9fa7b85fd32d7a06e7725dbe2a0ee
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2022-11-03 16:29:12 +0100

    scripts.cli: show aggregate stats

--- nmalign/scripts/cli.py
+++ nmalign/scripts/cli.py
@@ -41,13 +41,24 @@ def cli(processes, normalization, allow_splits, show_strings, show_files, separa
         strings2, files2, filelist2):
     """Force-align two lists of strings.
 
-    Computes string alignments between each pair among l1 and l2.
-    Then iteratively searches the next closest pair. Stores
-    the assigned result as a mapping from l1 to l2.
+    Computes string alignments between each pair among l1 and l2
+    (after optionally normalising both sides).
+
+    Then iteratively searches the next closest pair, while trying
+    to maintain local monotonicity.
+
+    If splits are allowed and the score is already low, then searches
+    for more matches among l1 for the pair's right side sequence:
+    If any subset of them can be combined into a path such that the
+    sum score is better than the integral score, then prefers those
+    assignments.
+
+    Stores the assigned result as a mapping from l1 to l2.
     (Unmatched or cut off elements will be assigned -1.)
 
     Prints the corresponding list indices and match scores [0.0,1.0]
-    as CSV data.
+    as CSV data. (For subsequences, the start and end position will
+    be appended.)
     """
     #list1 = list(map(file_.read() for file_ in files1))
     if strings1:
@@ -72,8 +83,12 @@ def cli(processes, normalization, allow_splits, show_strings, show_files, separa
         res_ind, res_beg, res_end = res
     else:
         res_ind = res
+    scores = []
+    match1 = set()
+    match2 = set()
     for ind1, ind2 in enumerate(res_ind):
-        score = str(dst[ind1])
+        score = dst[ind1]
+        scores.append(score)
         if show_strings:
             if ind2 < 0:
                 continue
@@ -89,10 +104,17 @@ def cli(processes, normalization, allow_splits, show_strings, show_files, separa
         else:
             a = str(ind1)
             b = str(ind2)
-        msg = a + separator + b + separator + score
+        msg = a + separator + b + separator + "%.2f" % score
         if allow_splits and res_beg[ind1] >= 0 and res_end[ind1] >= 0:
             msg += separator + str(res_beg[ind1]) + separator + str(res_end[ind1])
         click.echo(msg)
+        if ind2 < 0:
+            continue
+        match1.add(ind1)
+        match2.add(ind2)
+    click.echo("average alignment confidence: %d%%" % (100 * sum(scores) / len(scores)), err=True)
+    click.echo("coverage of matching inputs1: %d%%" % (100 * len(match1) / len(list1)), err=True)
+    click.echo("coverage of matching inputs2: %d%%" % (100 * len(match2) / len(list2)), err=True)
 
 if __name__ == '__main__':
     cli()
